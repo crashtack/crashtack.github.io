7604f9a9d380796c55f0cccf82fe1e3f40d303a1
crashtack <crashtack@gmail.com>
2016-05-29 12:59:19 -0700

    Update main.js

--- goforawalk/js/main.js
+++ goforawalk/js/main.js
@@ -57,4 +57,4 @@ RandomPerson.prototype.randomResponse = function() {
 
 var oldMan = new randomPerson("Old Man");
 
-alert("You meet an " + oldMan + " and he " + oldMan.randomResponse());
+alert("You meet an " + oldMan.name + " and he " + oldMan.randomResponse());
