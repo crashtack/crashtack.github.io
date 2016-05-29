ef0d898080242ce2a9a38a82b92ba7eb1ad484af
crashtack <crashtack@gmail.com>
2016-05-29 13:16:13 -0700

    Update main.js

--- goforawalk/js/main.js
+++ goforawalk/js/main.js
@@ -3,25 +3,25 @@ Title: Go For a Walk
 ------------------------------------------	
 It's 10am Saturday morning and it's time to drag your owner outside for a walk.
 Where will you go and what will you see?
-
 ----------
 Game World
 ----------
-
-							Home
-				Left						Right
-		Dognut			Coffee			Park	Around the block
+Locations
+----------
+Home(1)
+	Left to corner1(2): distance 3 blocks
+		Left to DognutShop(3): distance 2 blocks
+		Right to CoffeeShop(4): distance 1 block
+	Right to corner2(5): distance 1 block
+		Straight to Park(6): distance 4 blocks
+		Right to AroundTheBlock(7): distance 3 blocks
 
 Characters						Attributes
 ----------						----------
 - You (Dog)						- name, age, mood, happy responce(), angry responce(), say()
-
 - Other dogs					- name, age, breed, color, mood, happy responce(), angry responce(), say()
-
 - Random person					- sex, age (young, old), scared responce(), happy responce()
-
 - shop owner					- sex, age, discusted responce(), happy responce()
-
 Win and Losses
 --------------
 - Win: 	make it to the dognut shop and get a free dognut 
@@ -30,19 +30,17 @@ Win and Losses
 		Chase a squirl up a tree
 		
 - Lose:	Make it back home without having a dognut
-
 */
 
 alert("ready");
 
-/*
 var thisTest = new test("foobar");
 alert(thisTest.name);
 
 var Dude = new randomPerson("Dude");
 
 alert(Dude.responses.happy);
-*/
+
 /*
 RandomPerson.prototype.randomResponse = function() {
 	var randomNum = Math.floor((Math.random() * 2) +1);
@@ -55,5 +53,8 @@ RandomPerson.prototype.randomResponse = function() {
 };
 */
 
+
 var oldMan = new randomPerson("Old Man");
+
 alert("You meet an " + oldMan.name + " walking down the sidewalk. " + oldMan.randomResponse());
+
